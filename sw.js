2b2855d70e733a7fa98ebef09c282fbb3f492c12
Pleaseeeee-droid <daphnetull79@gmail.com>
2026-08-25 17:56:28 -0400

    Refresh SaveMoney cache for blockchain integration

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='savemoney-v4';
-const ASSETS=['./','./index.html','./styles.css?v=7','./app.js?v=9','./dwolla-bank-verify.js?v=1','./dwolla-transfer-test.js?v=2','./manifest.webmanifest','./icon.svg'];
+const CACHE='savemoney-v5';
+const ASSETS=['./','./index.html','./styles.css?v=10','./blockchain-vault.css?v=1','./app.js?v=9','./dwolla-bank-verify.js?v=1','./dwolla-transfer-test.js?v=3','./payment-methods.js?v=1','./funding-options.js?v=2','./blockchain-vault.js?v=1','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
