6c7b6806c9b7804702052989013f6f2b75f92c09
Pleaseeeee-droid <daphnetull79@gmail.com>
2026-08-25 15:10:41 -0400

    Refresh service worker cache for Dwolla transfer UI

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='savemoney-v3';
-const ASSETS=['./','./index.html','./styles.css?v=6','./app.js?v=6','./manifest.webmanifest','./icon.svg'];
+const CACHE='savemoney-v4';
+const ASSETS=['./','./index.html','./styles.css?v=7','./app.js?v=9','./dwolla-bank-verify.js?v=1','./dwolla-transfer-test.js?v=2','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
