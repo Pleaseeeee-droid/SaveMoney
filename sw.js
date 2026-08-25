981cf22e95ee6687d3fb6ebf0208a2942aea3599
Pleaseeeee-droid <daphnetull79@gmail.com>
2026-08-25 13:27:20 -0400

    Refresh cached database-backed app assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='savemoney-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='savemoney-v3';
+const ASSETS=['./','./index.html','./styles.css?v=6','./app.js?v=6','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
